06a80011d6bc228ac5a622f542c6de6d010558ec
Math-ieu <atsoumathieu228@gmail.com>
2026-04-06 16:57:41 +0100

    feat: add tag and gallery image handling to PostSerializer create and update methods

--- blog/serializers.py
+++ blog/serializers.py
@@ -58,6 +58,13 @@ class PostSerializer(serializers.ModelSerializer):
     author_details = AuthorSerializer(source="author", read_only=True)
     tags = TagSerializer(many=True, read_only=True)
     gallery = PostImageSerializer(many=True, read_only=True)
+    
+    # Write-only fields for complex related data
+    tag_names = serializers.ListField(
+        child=serializers.CharField(max_length=50),
+        write_only=True,
+        required=False
+    )
 
     class Meta:
         model = Post
@@ -65,7 +72,49 @@ class PostSerializer(serializers.ModelSerializer):
         read_only_fields = ["slug", "author", "published_at", "created_at", "updated_at"]
 
     def create(self, validated_data):
+        tag_names = validated_data.pop("tag_names", [])
         request = self.context.get("request")
         if request and request.user:
             validated_data["author"] = request.user
-        return super().create(validated_data)
+        
+        post = super().create(validated_data)
+        
+        # Handle tags
+        for name in tag_names:
+            tag, _ = Tag.objects.get_or_create(name=name)
+            post.tags.add(tag)
+            
+        # Handle gallery images from request.FILES
+        if request and request.FILES:
+            gallery_files = request.FILES.getlist('gallery_files')
+            for f in gallery_files:
+                PostImage.objects.create(post=post, image=f)
+
+        return post
+
+    def update(self, instance, validated_data):
+        tag_names = validated_data.pop("tag_names", None)
+        request = self.context.get("request")
+        
+        post = super().update(instance, validated_data)
+        
+        if tag_names is not None:
+            post.tags.clear()
+            for name in tag_names:
+                tag, _ = Tag.objects.get_or_create(name=name)
+                post.tags.add(tag)
+        
+        # Handle gallery updates (targeted addition and removal)
+        if request:
+            # 1. Remove specific images if delete_ids provided
+            delete_ids = request.data.getlist('delete_gallery_ids')
+            if delete_ids:
+                instance.gallery.filter(id__in=delete_ids).delete()
+            
+            # 2. Add new files from gallery_files
+            if request.FILES:
+                gallery_files = request.FILES.getlist('gallery_files')
+                for f in gallery_files:
+                    PostImage.objects.create(post=instance, image=f)
+                
+        return post
