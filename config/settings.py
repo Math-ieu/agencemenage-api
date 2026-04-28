b46ba745f71572ea4d7927fc30a0f4c05d501557
Math-ieu <atsoumathieu228@gmail.com>
2026-04-28 02:23:41 +0100

    feat: implement custom pagination with optional disable parameter and global configuration

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'config.pagination.CustomPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
